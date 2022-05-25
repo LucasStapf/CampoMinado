b1b4cd1c8c8e59c7810bb6aa689a2216f59d4833
LucasStapf <lucas.c.stapf@gmail.com>
2022-05-25 13:33:58 -0300

    Alterado modo de exibição do simbolo e melhorias no eventos do mouse.

--- src/main/java/com/stapf/campominado/celula/Celula.java
+++ src/main/java/com/stapf/campominado/celula/Celula.java
@@ -1,7 +1,6 @@
 package com.stapf.campominado.celula;
 
 import javafx.event.EventHandler;
-import javafx.event.EventType;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
@@ -9,11 +8,22 @@ import javafx.scene.layout.StackPane;
 public class Celula extends StackPane {
 
     /**
-     * Tamanho do lado da célula.
+     * Tamanho do lado da célula. Por padrão, o tamanho inicial é 20.
      */
     private int tamanhoLado = 20;
 
+    /**
+     * Número de minas ao redor da célula.
+     */
+    private int minasVizinhas = 0;
+
+    /**
+     * Indica qual simbolo será representado na célula.
+     */
     private Simbolo simbolo = Simbolo.PADRAO;
+
+    private Status status = Status.AGUARDANDO;
+
     public Celula() {
         setMinSize(tamanhoLado, tamanhoLado);
         setPrefSize(tamanhoLado, tamanhoLado);
@@ -22,18 +32,90 @@ public class Celula extends StackPane {
         atualizaSimbolo();
     }
 
+    public Simbolo getSimbolo() {
+        return simbolo;
+    }
+
+    public void setSimbolo(Simbolo simbolo) {
+        this.simbolo = simbolo;
+        atualizaSimbolo();
+    }
+
     private void atualizaSimbolo() {
+
         getChildren().clear();
-        getChildren().add(simbolo.getSimbolo());
+
+        if (status == Status.AGUARDANDO) {
+            getChildren().add(Simbolo.PADRAO.getSimbolo());
+        } else if (status == Status.BLOQUEADA) {
+            getChildren().add(Simbolo.BANDEIRA.getSimbolo());
+        } else if (status == Status.ATIVADA) {
+            if (!minado) {
+                switch (minasVizinhas) {
+                    case 1 -> getChildren().add(Simbolo.UM.getSimbolo());
+                    case 2 -> getChildren().add(Simbolo.DOIS.getSimbolo());
+                    case 3 -> getChildren().add(Simbolo.TRES.getSimbolo());
+                    case 4 -> getChildren().add(Simbolo.QUATRO.getSimbolo());
+                    case 5 -> getChildren().add(Simbolo.CINCO.getSimbolo());
+                    case 6 -> getChildren().add(Simbolo.SEIS.getSimbolo());
+                    case 7 -> getChildren().add(Simbolo.SETE.getSimbolo());
+                    case 8 -> getChildren().add(Simbolo.OITO.getSimbolo());
+                    default -> getChildren().add(Simbolo.PRESSIONADO.getSimbolo());
+                }
+            } else getChildren().add(Simbolo.BOMBA.getSimbolo());
+        }
     }
 
     private EventHandler<MouseEvent> mouseEvent = new EventHandler<MouseEvent>() {
         @Override
         public void handle(MouseEvent mouseEvent) {
-            if (mouseEvent.getButton() == MouseButton.PRIMARY) simbolo = Simbolo.PRESSIONADO;
-            else if (mouseEvent.getButton() == MouseButton.SECONDARY) simbolo = Simbolo.BANDEIRA;
-            atualizaSimbolo();
+
+            if (mouseEvent.getButton() == MouseButton.PRIMARY) {
+
+                if (status != Status.AGUARDANDO) return;
+                else status = Status.ATIVADA;
+
+                atualizaSimbolo();
+
+            } else if (mouseEvent.getButton() == MouseButton.SECONDARY) {
+
+                if (status == Status.AGUARDANDO) {
+
+                    status = Status.BLOQUEADA;
+                    simbolo = Simbolo.BANDEIRA;
+                    atualizaSimbolo();
+
+                } else if (status == Status.BLOQUEADA) {
+
+                    status = Status.AGUARDANDO;
+                    simbolo = Simbolo.PADRAO;
+                    atualizaSimbolo();
+
+                }
+            }
         }
     };
+
+    /**
+     * Resposável por guardar a informação se existe uma mina ou não na célula.
+     */
+    private boolean minado = false;
+
+    /**
+     * Retorna verdadeiro se a célula possui mina, falso caso contrário.
+     * @return verdadeiro se a célula possui mina, falso caso contrário.
+     */
+    public boolean isMinado() {
+        return minado;
+    }
+
+    /**
+     * Altera a existência ou não de mina na célula.
+     * @param minado
+     */
+    public void setMinado(boolean minado) {
+        this.minado = minado;
+    }
+
 
 }
